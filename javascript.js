9e089315d45bf887cdf49bed0a87038fe9fd3930
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-06 16:13:06 +0000

    added javascript to create 16x16 grid
    spacing and styling not yet fixed

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 // create grid of 16 square divs
 const cont = document.querySelector(".container");
 
-for (let i=0; i<16; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("gridSquare");
-    cont.appendChild(gridDiv)
+for (let j=0; j<16; j++) { 
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("gridRow");
+    for (let i=0; i<16; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.classList.add("gridSquare");
+        gridRow.appendChild(gridDiv)
+    }
+    cont.appendChild(gridRow)
 }
-
 
