7e82ba05dd8cf01c6dbf3270aea6ae6654af5b7f
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-06 16:01:40 +0000

    Added js to create one grid square and basic style

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
 // create grid of 16 square divs
-const cont = document.getElementsByClassName(".container");
+const cont = document.querySelector(".container");
+const gridDiv = document.createElement("div");
+gridDiv.classList.add("gridSquare");
+
+cont.appendChild(gridDiv)
