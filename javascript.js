fbd170b066ed8ac06c8a6369852846f21b1a26c3
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-06 16:04:29 +0000

    added loop to create 16 divs

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 // create grid of 16 square divs
 const cont = document.querySelector(".container");
-const gridDiv = document.createElement("div");
-gridDiv.classList.add("gridSquare");
 
-cont.appendChild(gridDiv)
+for (let i=0; i<16; i++) {
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("gridSquare");
+    cont.appendChild(gridDiv)
+}
+
+
