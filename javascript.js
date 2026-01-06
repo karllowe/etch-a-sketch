142ce976d0736d64cfa9ffb92463744486906a39
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-06 16:46:25 +0000

    button click now prompts for grid size
    Updated button to prompt user for grid size, and
    updated js functions to use this new size.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 // create grid of 16 square divs
-function createGrid() {
+function createGrid(size) {
     const cont = document.querySelector(".container");
-    for (let j=0; j<16; j++) { 
+    for (let j=0; j<size; j++) { 
         const gridRow = document.createElement("div");
         gridRow.classList.add("gridRow");
-        for (let i=0; i<16; i++) {
+        for (let i=0; i<size; i++) {
             const gridDiv = document.createElement("div");
             gridDiv.classList.add("gridSquare");
             gridDiv.addEventListener("mouseover", () => {
@@ -25,10 +25,11 @@ function removeGrid() {
     }
 }
 
-createGrid();
+createGrid(16);
 // reset grid button
 const resetButton = document.querySelector("button");
 resetButton.addEventListener("click",() => {
+    let newSize = prompt("How many boxes?");
     removeGrid();
-    createGrid();
+    createGrid(newSize);
 })
